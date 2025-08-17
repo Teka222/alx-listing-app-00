ecf8360817f6bf24465ca0ca5ae6ff9549d6e348
Tekano Meso <tekanonetflorist01@gmail.com>
2025-08-17 15:09:18 +0200

    Create Footer.tsx

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer className="bg-gray-100 p-4 text-center text-sm text-gray-600">
+    &copy; 2024 ListingApp. All rights reserved.
+  </footer>
+);
+
+export default Footer;
